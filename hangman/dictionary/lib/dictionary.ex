4787fcacba66e5da6a1a866b1fa74f27bb3e9a66
Ublo <Ublo@MacBookAir-Ublo-Alejo.local>
2025-01-23 10:51:30 +0100

    a mad dash - regular expressions

--- hangman/dictionary/lib/dictionary.ex
+++ hangman/dictionary/lib/dictionary.ex
@@ -12,23 +12,40 @@ defmodule Dictionary do
   end
 
 
-  def exo() do
+  def exo_manipulation_string() do
 
     str = "had we but world enough, and time"
 
     str_split = String.split(str, ",", trim: true)
     str_chars = String.graphemes(str)
     str_int   = String.to_charlist("olá")
-    
+
     str_reverse = String.reverse(str)
 
     {str_split, str_chars, str_int, str_reverse}
 
 
   end
+
+  def explore_regex(str) do
+    reg1 = Regex.match?(~r/a.c/, str)
+
+    reg2 = String.replace(str, ~r/cat/, "dog")
+
+
+    {reg1, reg2}
+
+  end
 end
+
+
+
+
+
+
 
 
 # IO.inspect Dictionary.word_list()
 # IO.inspect Dictionary.random_word()
-IO.inspect Dictionary.exo()
+IO.inspect Dictionary.explore_regex("abc cat")
+IO.inspect Dictionary.explore_regex("ace cat")
