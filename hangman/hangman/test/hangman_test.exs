cd6c9b3c1e7a8a223b5758d1d23f079ae3ceabe6
Nazim HADJ SADOK <nazim@ublo.immo>
2025-01-24 12:01:27 +0100

    let's start coding - pattern matching game state

--- hangman/hangman/test/hangman_test.exs
+++ hangman/hangman/test/hangman_test.exs
@@ -19,18 +19,34 @@ test "new game returns correct word" do
   assert game.game_state == :initializing
   assert game.letters == ["w", "o", "m", "b", "a", "t"]
 
-  
+
 end
 
 
 test "lower case in the game letters" do
   game = Game.new_game("wombat")
-  assert Enum.all?(game.letters, fn letter -> String.match?(letter, ~r/[a-z]/)
+  assert Enum.all?(game.letters, fn letter -> String.match?(letter, ~r/[a-z]/) end)
 
-end)
 end
 
 
+test "state doesn't change if game is won or lost" do
+  game = Game.new_game("wombat")
+
+  for state <- [:won, :lost] do
+
+  game = Map.put(game, :game_state, state)
+
+  {new_game, _tally} = Game.make_move(game, "x")
+
+  assert new_game == game
+
+  end
+
+
+
+
+end
 
 
 end
